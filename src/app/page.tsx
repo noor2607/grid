af28453f54a56da222947a1eb59b5b61b9a50325
Lareb Noor <156116687+noor2607@users.noreply.github.com>
2025-01-30 15:24:26 +0500

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[auto_1fr_1fr_1fr_auto] grid-cols-[1fr_3fr] gap-4 h-screen">
+    <div className="grid grid-rows-[auto_1fr_auto] grid-cols-[40%_60%] gap-4 h-screen">
       <header className="bg-yellow-500 p-6 text-center text-xl font-bold col-span-2">
         Header
       </header>
 
-      <aside className="bg-blue-500 text-white p-6">
+      <aside className="bg-blue-500 text-white p-10">
         Sidebar
       </aside>
 
-      <div className="grid grid-rows-3 gap-4 p-6">
+      <div className="grid grid-rows-[auto_auto] gap-4 p-6">
         <div className="p-4 border border-gray-300">Content 1</div>
-        <div className="p-4 border border-gray-300">Content 2</div>
-        <div className="p-4 border border-gray-300">Content 3</div>
+
+        <div className="grid grid-cols-2 gap-4">
+          <div className="p-4 border border-gray-300">Content 2</div>
+          <div className="p-4 border border-gray-300">Content 3</div>
+        </div>
       </div>
 
       <footer className="bg-gray-800 text-white p-6 text-center col-span-2">
         Footer
       </footer>
     </div>
-  
-
   );
 }
